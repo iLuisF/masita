ffdfaa6c41f2127188a9e1045ce8f941e3f3d443
droogadulce <ahigareda.mayra@gmail.com>
2017-03-20 19:10:10 -0600

    Creación de las tablas

--- SQL/proyecto-comida.sql
+++ SQL/proyecto-comida.sql
@@ -1,1 +1,149 @@
---PRUEBA
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:8889
+-- Tiempo de generación: 21-03-2017 a las 02:07:43
+-- Versión del servidor: 5.6.34
+-- Versión de PHP: 7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `Masita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Comentario`
+--
+
+CREATE TABLE `Comentario` (
+  `idComentario` int(11) NOT NULL,
+  `idPuesto` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `contenido` varchar(300) NOT NULL,
+  `fecha` date NOT NULL,
+  `calificacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Puesto`
+--
+
+CREATE TABLE `Puesto` (
+  `idPuesto` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL,
+  `tipoComida` int(11) NOT NULL,
+  `horario` varchar(16) NOT NULL,
+  `ubicacion` varchar(64) NOT NULL,
+  `idServicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ServicioAdicional`
+--
+
+CREATE TABLE `ServicioAdicional` (
+  `idServicio` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `TipoComida`
+--
+
+CREATE TABLE `TipoComida` (
+  `idTipoComida` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `UsuarioCiencias`
+--
+
+CREATE TABLE `UsuarioCiencias` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(64) NOT NULL,
+  `app` varchar(64) NOT NULL,
+  `apm` varchar(64) NOT NULL,
+  `correo` varchar(128) NOT NULL,
+  `nombreUsuario` varchar(64) NOT NULL,
+  `contrasenia` varchar(128) NOT NULL,
+  `activo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Comentario`
+--
+ALTER TABLE `Comentario`
+  ADD PRIMARY KEY (`idComentario`);
+
+--
+-- Indices de la tabla `Puesto`
+--
+ALTER TABLE `Puesto`
+  ADD PRIMARY KEY (`idPuesto`);
+
+--
+-- Indices de la tabla `ServicioAdicional`
+--
+ALTER TABLE `ServicioAdicional`
+  ADD PRIMARY KEY (`idServicio`);
+
+--
+-- Indices de la tabla `TipoComida`
+--
+ALTER TABLE `TipoComida`
+  ADD PRIMARY KEY (`idTipoComida`);
+
+--
+-- Indices de la tabla `UsuarioCiencias`
+--
+ALTER TABLE `UsuarioCiencias`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Comentario`
+--
+ALTER TABLE `Comentario`
+  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `Puesto`
+--
+ALTER TABLE `Puesto`
+  MODIFY `idPuesto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ServicioAdicional`
+--
+ALTER TABLE `ServicioAdicional`
+  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `TipoComida`
+--
+ALTER TABLE `TipoComida`
+  MODIFY `idTipoComida` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `UsuarioCiencias`
+--
+ALTER TABLE `UsuarioCiencias`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
