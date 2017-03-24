69b2156e3d2a547ac0219a2aad4c4cbd4b0adbad
droogadulce <ahigareda.mayra@gmail.com>
2017-03-23 22:09:55 -0600

    Actualización del script de mysql
    Se añaden los cambios al script para tener la base de datos ya normalizada, con las restricciones de integridad referencial correspondientes.

--- SQL/proyecto-comida.sql
+++ SQL/proyecto-comida.sql
@@ -1,142 +1,101 @@
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 --
--- Base de datos: `Masita`
+-- Base de datos: masita
 --
 create database masita;
 
 use masita;
-
--- --------------------------------------------------------
-
-CREATE TABLE UsuarioCiencias (
-	id_usuario 					int NOT NULL,
-    contrasenia 				varchar(128) NOT NULL,
-    nombre 						varchar(64) NOT NULL,
-    app 						varchar(64) NOT NULL,
-    apm 						varchar(64) NOT NULL,
-    activo 						varchar(128) NOT NULL,
-    nombre_usuario 				varchar(64) NOT NULL,
-    correo 						varchar(128) NOT NULL,
-    PRIMARY KEY(id_usuario)
-) ; ENGINE=InnoBD DEFAULT CHARSET=utf8;
-
-
 --
--- Estructura de tabla para la tabla `Comentario`
+-- Estructura para la tabla Usuario
 --
-
-CREATE TABLE `Comentario` (
-  `idComentario` int(11) NOT NULL,
-  `idPuesto` int(11) NOT NULL,
-  `idUsuario` int(11) NOT NULL,
-  `contenido` varchar(300) NOT NULL,
-  `fecha` date NOT NULL,
-  `calificacion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+CREATE TABLE Usuario (
+  idUsuario       int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  correo          varchar(128) NOT NULL,
+  contrasenia     varchar(128) NOT NULL,
+  nombre          varchar(64) NOT NULL,
+  app             varchar(64) NOT NULL,
+  apm             varchar(64) NOT NULL,
+  activo          varchar(128) NOT NULL,
+  nombreUsuario   varchar(64) NOT NULL,
+  esAdministrador int(1) NOT NULL
+);
 
 --
--- Estructura de tabla para la tabla `Puesto`
+-- Estructura para la tabla Puesto
 --
-
-CREATE TABLE `Puesto` (
-  `idPuesto` int(11) NOT NULL,
-  `nombre` varchar(64) NOT NULL,
-  `tipoComida` int(11) NOT NULL,
-  `horario` varchar(16) NOT NULL,
-  `ubicacion` varchar(64) NOT NULL,
-  `idServicio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+CREATE TABLE Puesto (
+  idPuesto      int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre        varchar(64) NOT NULL,
+  horario       varchar(16) NOT NULL,
+  ubicacion     varchar(64) NOT NULL
+);
 
 --
--- Estructura de tabla para la tabla `ServicioAdicional`
+-- Estructura para la tabla Comentario
 --
-
-CREATE TABLE `ServicioAdicional` (
-  `idServicio` int(11) NOT NULL,
-  `nombre` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `TipoComida`
---
-
-CREATE TABLE `TipoComida` (
-  `idTipoComida` int(11) NOT NULL,
-  `nombre` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+CREATE TABLE Comentario (
+  idComentario    int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  idPuesto        int NOT NULL,
+  idUsuario       int NOT NULL,
+  contenido       varchar(300),
+  fecha           date NOT NULL,
+  calificacion    int(11)
+);
 
 --
--- Índices para tablas volcadas
+-- Estructura para la tabla TipoComida
 --
+CREATE TABLE TipoComida (
+  idTipoComida  int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre        varchar(64) NOT NULL
+);
 
 --
--- Indices de la tabla `Comentario`
+-- Estructura para la tabla ServicioAdicional
 --
-ALTER TABLE `Comentario`
-  ADD PRIMARY KEY (`idComentario`);
+CREATE TABLE ServicioAdicional (
+  idServicio    int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre        varchar(64) NOT NULL
+);
 
 --
--- Indices de la tabla `Puesto`
+-- Estructura para la tabla TipoComidaPuesto
 --
-ALTER TABLE `Puesto`
-  ADD PRIMARY KEY (`idPuesto`);
+CREATE TABLE TipoComidaPuesto (
+  idTipoComida  int NOT NULL,
+  idPuesto      int NOT NULL,
+  PRIMARY KEY (idTipoComida, idPuesto)
+);
 
 --
--- Indices de la tabla `ServicioAdicional`
+-- Estructura para la tabla ServicioPuesto
 --
-ALTER TABLE `ServicioAdicional`
-  ADD PRIMARY KEY (`idServicio`);
+CREATE TABLE ServicioAdicionalPuesto (
+  idServicio    int NOT NULL,
+  idPuesto      int NOT NULL,
+  PRIMARY KEY (idServicio, idPuesto)
+);
+
+-- ---------------------------------------------
 
 --
--- Indices de la tabla `TipoComida`
+-- Indices de la tabla Usuario
 --
-ALTER TABLE `TipoComida`
-  ADD PRIMARY KEY (`idTipoComida`);
+ALTER TABLE Usuario ADD UNIQUE KEY correo (correo);
 
 --
--- Indices de la tabla `UsuarioCiencias`
+-- Indices de la tabla Comentario
 --
-ALTER TABLE `UsuarioCiencias`
-  ADD PRIMARY KEY (`idUsuario`),
-  ADD UNIQUE KEY `correo` (`correo`);
+ALTER TABLE Comentario ADD CONSTRAINT fk_Comentario_idPuesto FOREIGN KEY (idPuesto) REFERENCES Puesto (idPuesto) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Comentario ADD CONSTRAINT fk_Comentario_idUsuario FOREIGN KEY (idUsuario) REFERENCES Usuario (idUsuario) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indices de la tabla TipoComidaPuesto
 --
+ALTER TABLE TipoComidaPuesto ADD CONSTRAINT fk_TipoComidaPuesto_idTipoComida FOREIGN KEY (idTipoComida) REFERENCES TipoComida (idTipoComida) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE TipoComidaPuesto ADD CONSTRAINT fk_TipoComidaPuesto_idPuesto FOREIGN KEY (idPuesto) REFERENCES Puesto (idPuesto) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- AUTO_INCREMENT de la tabla `Comentario`
+-- Indices de la tabla ServicioAdicionalPuesto
 --
-ALTER TABLE `Comentario`
-  MODIFY `idComentario` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `Puesto`
---
-ALTER TABLE `Puesto`
-  MODIFY `idPuesto` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `ServicioAdicional`
---
-ALTER TABLE `ServicioAdicional`
-  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `TipoComida`
---
-ALTER TABLE `TipoComida`
-  MODIFY `idTipoComida` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `UsuarioCiencias`
---
-ALTER TABLE `UsuarioCiencias`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE ServicioAdicionalPuesto ADD CONSTRAINT fk_ServicioAdicionalPuesto_idServicio FOREIGN KEY (idServicio) REFERENCES ServicioAdicional (idServicio) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE ServicioAdicionalPuesto ADD CONSTRAINT fk_ServicioAdicionalPuesto_idPuesto FOREIGN KEY (idPuesto) REFERENCES Puesto (idPuesto) ON DELETE CASCADE ON UPDATE CASCADE;
