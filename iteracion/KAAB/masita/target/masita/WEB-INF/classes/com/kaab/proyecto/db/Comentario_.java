7a8f5b88bebf56f19c9b25f05d1e7fafae8176f9
ErnestoRubenPalaciosGomez <ernesto_palacios@ciencias.unam.mx>
2017-04-23 05:39:19 -0500

    Casos de Uso Agregar,Editar y Eliminar Puestos
    Casos de Uso terminados y funcionales

--- iteracion/KAAB/masita/target/masita/WEB-INF/classes/com/kaab/proyecto/db/Comentario_.java
+++ iteracion/KAAB/masita/target/masita/WEB-INF/classes/com/kaab/proyecto/db/Comentario_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-20T02:19:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-23T05:36:40")
 @StaticMetamodel(Comentario.class)
 public class Comentario_ { 
 
