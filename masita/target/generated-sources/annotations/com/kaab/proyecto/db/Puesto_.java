2a5234a8d6c63cca4ed9e78298a5ccb9acd51b4a
Luis Flores González <iluis@ciencias.unam.mx>
2017-05-14 23:10:09 -0500

    Se agrego estilo de ControladorComentario además de limpiar el código.

--- masita/target/generated-sources/annotations/com/kaab/proyecto/db/Puesto_.java
+++ masita/target/generated-sources/annotations/com/kaab/proyecto/db/Puesto_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-14T22:16:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-14T22:51:46")
 @StaticMetamodel(Puesto.class)
 public class Puesto_ { 
 
