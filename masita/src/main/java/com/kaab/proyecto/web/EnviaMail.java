d1e1cb797e65ae1dd765c1413a254b0bc46cf2be
Javier <javiertellezvieyra@ciencias.unam.mx>
2017-05-30 13:30:14 -0500

    Agrega checkstyle.

--- masita/src/main/java/com/kaab/proyecto/web/EnviaMail.java
+++ masita/src/main/java/com/kaab/proyecto/web/EnviaMail.java
@@ -24,24 +24,19 @@ import javax.persistence.Persistence;
 public class EnviaMail implements Serializable {
     private final EntityManagerFactory emf
             = Persistence.createEntityManagerFactory("MiProyectoPU");
-     private final UsuarioJpaController controlador = new UsuarioJpaController(emf);
-    
-    
-    
-    
+     private final UsuarioJpaController controlador =
+             new UsuarioJpaController(emf);
      public void envia(String correo){
-       // La dirección de envío (to)
+       // La direcciï¿½n de envï¿½o (to)
        String para = correo;
 
-       // La dirección de la cuenta de envío (from)
+       // La direcciï¿½n de la cuenta de envï¿½o (from)
        String de = "kaabcompany@gmail.com";
        // El servidor (host). En este caso usamos localhost
        String host = "localhost";
 
        // Obtenemos las propiedades del sistema
        Properties propiedades = System.getProperties();
-        
-
        // Configuramos el servidor de correo
        propiedades.setProperty("mail.smtp.host", host);
        propiedades.put("mail.smtp.auth", "true");
@@ -49,27 +44,27 @@ public class EnviaMail implements Serializable {
        propiedades.put("mail.smtp.host", "smtp.gmail.com");
        propiedades.put("mail.smtp.port", "587");
 
-       // Obtenemos la sesión por defecto
+       // Obtenemos la sesiï¿½n por defecto
         Session sesion = Session.getInstance(propiedades,
           new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(de, "seguridadImportante");
             }
           });
-       try{
+       try {
          // Creamos un objeto mensaje tipo MimeMessage por defecto.
          MimeMessage mensaje = new MimeMessage(sesion);
 
-         // Asignamos el “de o from” al header del correo.
+         // Asignamos el ï¿½de o fromï¿½ al header del correo.
          mensaje.setFrom(new InternetAddress(de));
 
-         // Asignamos el “para o to” al header del correo.
+         // Asignamos el ï¿½para o toï¿½ al header del correo.
          mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(para));
 
          // Asignamos el asunto
          mensaje.setSubject("Validacion de cuenta");
          String aux = "<h1>presione el link para proceder a validar su cuenta</h1>"
-                             + "<a href=http://localhost:8084/masita/CuentaValidadaIH.xhtml?idUsuario=" + buscaid(para) + "> presione aqui para ir a la página<a/>";
+                             + "<a href=http://localhost:8084/masita/CuentaValidadaIH.xhtml?idUsuario=" + buscaid(para) + "> presione aqui para ir a la pï¿½gina<a/>";
          System.out.println(aux);
          // Asignamos el mensaje como tal
           mensaje.setContent(aux ,"text/html" );
